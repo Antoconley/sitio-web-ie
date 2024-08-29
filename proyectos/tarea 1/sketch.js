a89d259993f2fa0f8741238850cd334d5795619b
Antoconley <antoconley@gmail.com>
2024-08-29 15:02:45 -0400

    Initial commit

---
+++ proyectos/tarea 1/sketch.js
@@ -0,0 +1,447 @@
+function setup() {
+  createCanvas(530, 460);
+  noStroke();
+}
+
+function draw() {
+  background(220);
+  
+  
+  //cuadrados grandes
+
+  //primera fila
+  fill("#82278c");
+  rect(5,5,70,70);
+
+  fill("black");
+  rect(80,5,70,70);
+  
+  fill("#82278c");
+  rect(155,5,70,70);
+
+  fill("black");
+  rect(230,5,70,70);
+  
+  fill("#82278c");
+  rect(305,5,70,70);
+  
+  fill("black");
+  rect(380,5,70,70);
+  
+  fill("#82278c");
+  rect(455,5,70,70);
+  
+
+  //segunda fila
+  fill("black");
+  rect(5,80,70,70);
+
+  fill("#03ab60");
+  rect(80,80,70,70);
+  
+  fill("black");
+  rect(155,80,70,70);
+
+  fill("#00d8db");
+  rect(230,80,70,70);
+  
+  fill("black");
+  rect(305,80,70,70);
+  
+  fill("#03ab60");
+  rect(380,80,70,70);
+  
+  fill("black");
+  rect(455,80,70,70);
+  
+  
+  
+  
+    //tercera fila
+  fill("#82278c");
+  rect(5,155,70,70);
+
+  fill("black");
+  rect(80,155,70,70);
+  
+  fill("#82278c");
+  rect(155,155,70,70);
+
+  fill("black");
+  rect(230,155,70,70);
+  
+  fill("#82278c");
+  rect(305,155,70,70);
+  
+  fill("black");
+  rect(380,155,70,70);
+  
+  fill("#82278c");
+  rect(455,155,70,70);
+  
+  
+  
+
+  //cuarta fila
+  fill("black");
+  rect(5,230,70,70);
+
+  fill("#00d8db");
+  rect(80,230,70,70);
+  
+  fill("black");
+  rect(155,230,70,70);
+
+  fill("#03ab60");
+  rect(230,230,70,70);
+  
+  fill("black");
+  rect(305,230,70,70);
+  
+  fill("#00d8db");
+  rect(380,230,70,70);
+  
+  fill("black");
+  rect(455,230,70,70);
+  
+  
+  
+  
+      //quinta fila
+  fill("#82278c");
+  rect(5,305,70,70);
+
+  fill("black");
+  rect(80,305,70,70);
+  
+  fill("#82278c");
+  rect(155,305,70,70);
+
+  fill("black");
+  rect(230,305,70,70);
+  
+  fill("#82278c");
+  rect(305,305,70,70);
+  
+  fill("black");
+  rect(380,305,70,70);
+  
+  fill("#82278c");
+  rect(455,305,70,70);
+  
+  
+  
+   //sexta fila
+  fill("black");
+  rect(5,380,70,70);
+
+  fill("#03ab60");
+  rect(80,380,70,70);
+  
+  fill("black");
+  rect(155,380,70,70);
+
+  fill("#00d8db");
+  rect(230,380,70,70);
+  
+  fill("black");
+  rect(305,380,70,70);
+  
+  fill("#03ab60");
+  rect(380,380,70,70);
+  
+  fill("black");
+  rect(455,380,70,70);
+  
+ 
+  
+  //cuadrados medianos
+  
+  //primera fila 2
+  fill("#872cd1");
+  rect(15,15,50,50);
+
+  fill("#c782ff");
+  rect(90,15,50,50);
+  
+  fill("#872cd1");
+  rect(165,15,50,50);
+
+  fill("#c782ff");
+  rect(240,15,50,50);
+  
+  fill("#872cd1");
+  rect(315,15,50,50);
+  
+  fill("#c782ff");
+  rect(390,15,50,50);
+  
+  fill("#872cd1");
+  rect(465,15,50,50);
+  
+  
+    //segunda fila 2
+  fill("white");
+  rect(15,90,50,50);
+
+  fill("black");
+  rect(90,90,50,50);
+  
+  fill("white");
+  rect(165,90,50,50);
+
+  fill("black");
+  rect(240,90,50,50);
+  
+  fill("white");
+  rect(315,90,50,50);
+  
+  fill("black");
+  rect(390,90,50,50);
+  
+  fill("white");
+  rect(465,90,50,50);
+  
+  
+  
+    //tercera fila 2
+  fill("#872cd1");
+  rect(15,165,50,50);
+
+  fill("#c782ff");
+  rect(90,165,50,50);
+  
+  fill("#872cd1");
+  rect(165,165,50,50);
+
+  fill("#c782ff");
+  rect(240,165,50,50);
+  
+  fill("#872cd1");
+  rect(315,165,50,50);
+  
+  fill("#c782ff");
+  rect(390,165,50,50);
+  
+  fill("#872cd1");
+  rect(465,165,50,50);
+  
+  
+    //cuarta fila 2
+  fill("white");
+  rect(15,240,50,50);
+
+  fill("black");
+  rect(90,240,50,50);
+  
+  fill("white");
+  rect(165,240,50,50);
+
+  fill("black");
+  rect(240,240,50,50);
+  
+  fill("white");
+  rect(315,240,50,50);
+  
+  fill("black");
+  rect(390,240,50,50);
+  
+  fill("white");
+  rect(465,240,50,50);
+  
+  
+      //quinta fila 2
+  fill("#872cd1");
+  rect(15,315,50,50);
+
+  fill("#c782ff");
+  rect(90,315,50,50);
+  
+  fill("#872cd1");
+  rect(165,315,50,50);
+
+  fill("#c782ff");
+  rect(240,315,50,50);
+  
+  fill("#872cd1");
+  rect(315,315,50,50);
+  
+  fill("#c782ff");
+  rect(390,315,50,50);
+  
+  fill("#872cd1");
+  rect(465,315,50,50);
+  
+  
+    //sexta fila 2
+  fill("white");
+  rect(15,390,50,50);
+
+  fill("black");
+  rect(90,390,50,50);
+  
+  fill("white");
+  rect(165,390,50,50);
+
+  fill("black");
+  rect(240,390,50,50);
+  
+  fill("white");
+  rect(315,390,50,50);
+  
+  fill("black");
+  rect(390,390,50,50);
+  
+  fill("white");
+  rect(465,390,50,50);
+  
+  
+  
+  
+    //cuadrados pequeños
+  
+  //primera fila 3
+  fill("black");
+  rect(25,25,30,30);
+
+  fill("#872cd1");
+  rect(100,25,30,30);
+  
+  fill("black");
+  rect(175,25,30,30);
+
+  fill("#872cd1");
+  rect(250,25,30,30);
+  
+  fill("black");
+  rect(325,25,30,30);
+  
+  fill("#872cd1");
+  rect(400,25,30,30);
+  
+  fill("black");
+  rect(475,25,30,30);
+  
+  
+  
+  
+    //segunda fila 3
+  fill("black");
+  rect(25,100,30,30);
+
+  fill("#124224");
+  rect(100,100,30,30);
+  
+  fill("black");
+  rect(175,100,30,30);
+
+  fill("#104747");
+  rect(250,100,30,30);
+  
+  fill("black");
+  rect(325,100,30,30);
+  
+  fill("#124224");
+  rect(400,100,30,30);
+  
+  fill("black");
+  rect(475,100,30,30);
+  
+  
+  
+    //tercera fila 3
+  fill("black");
+  rect(25,175,30,30);
+
+  fill("#872cd1");
+  rect(100,175,30,30);
+  
+  fill("black");
+  rect(175,175,30,30);
+
+  fill("#872cd1");
+  rect(250,175,30,30);
+  
+  fill("black");
+  rect(325,175,30,30);
+  
+  fill("#872cd1");
+  rect(400,175,30,30);
+  
+  fill("black");
+  rect(475,175,30,30);
+  
+  
+  
+  
+    //cuarta fila 3
+  fill("black");
+  rect(25,250,30,30);
+
+  fill("#104747");
+  rect(100,250,30,30);
+  
+  fill("black");
+  rect(175,250,30,30);
+
+  fill("#124224");
+  rect(250,250,30,30);
+  
+  fill("black");
+  rect(325,250,30,30);
+  
+  fill("#104747");
+  rect(400,250,30,30);
+  
+  fill("black");
+  rect(475,250,30,30);
+  
+  
+      //quinta fila 2
+  fill("black");
+  rect(25,325,30,30);
+
+  fill("#872cd1");
+  rect(100,325,30,30);
+  
+  fill("black");
+  rect(175,325,30,30);
+
+  fill("#872cd1");
+  rect(250,325,30,30);
+  
+  fill("black");
+  rect(325,325,30,30);
+  
+  fill("#872cd1");
+  rect(400,325,30,30);
+  
+  fill("black");
+  rect(475,325,30,30);
+  
+  
+  
+  
+    //sexta fila 3
+  fill("black");
+  rect(25,400,30,30);
+
+  fill("#124224");
+  rect(100,400,30,30);
+  
+  fill("black");
+  rect(175,400,30,30);
+
+  fill("#104747");
+  rect(250,400,30,30);
+  
+  fill("black");
+  rect(325,400,30,30);
+  
+  fill("#124224");
+  rect(400,400,30,30);
+  
+  fill("black");
+  rect(475,400,30,30);
+}
+  
